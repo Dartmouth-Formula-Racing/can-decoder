5c4063248da3f5ebb0e895665530d3b6b377f224
Henry Scheible <henry.scheible@gmail.com>
2023-01-31 19:32:30 -0500

    Changes by mainlinevip

--- candecoder/inverter_funcs.py
+++ candecoder/inverter_funcs.py
@@ -3,16 +3,16 @@ from candecoder.inverter_types import signed_int
 
 def temperatures1(data):
     return {
-        "ModA": (signed_int(data[0], data[1]), "degC"),
-        "ModB": (signed_int(data[2], data[3]), "degC"),
-        "ModC": (signed_int(data[4], data[5]), "degC"),
-        "GateDriverBoard": (signed_int(data[6], data[7]), "degC"),
+        "ModA": (signed_int(data[0], data[1]) / 10., "degC"),
+        "ModB": (signed_int(data[2], data[3]) / 10., "degC"),
+        "ModC": (signed_int(data[4], data[5]) / 10., "degC"),
+        "GateDriverBoard": (signed_int(data[6], data[7]) / 10., "degC"),
     }
 
 def temperatures2(data):
     return {
-        "ControlBoardTemperature": (signed_int(data[0], data[1]), "degC"),
-        "RTD1Temperature": (signed_int(data[2], data[3]), "degC"),
-        "RTD2Temperature": (signed_int(data[4], data[5]), "degC"),
-        "RTD3Temperature": (signed_int(data[6], data[7]), "degC")
+        "ControlBoardTemperature": (signed_int(data[0], data[1]) / 10., "degC"),
+        "RTD1Temperature": (signed_int(data[2], data[3]) / 10., "degC"),
+        "RTD2Temperature": (signed_int(data[4], data[5]) / 10., "degC"),
+        "RTD3Temperature": (signed_int(data[6], data[7]) / 10., "degC")
     }
