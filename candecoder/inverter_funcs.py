357967335a642958642b626607729791d2aa2f0a
cadler22 <calista.adler@gmail.com>
2023-02-07 19:01:59 -0500

    Inverter func

--- candecoder/inverter_funcs.py
+++ candecoder/inverter_funcs.py
@@ -92,3 +92,11 @@ def high_speed_message(data):
         "MotorSpeed": (signed_int(data[4], data[5]) / 10., "rpm"),
         "DCBusVoltage": (signed_int(data[6], data[7]) / 10., "volts")
     }
+
+
+def extract_by_range(data, start_bit, stop_bit):
+    x = data[0] + 256 * data[1] + 256**2 * data[2] + 256**3 * data[3] + 256**4 * data[4] + 256**5 * data[5] + 256**6 * data[6] + 256**7 * data[7]
+    base_mask = 2 ** (stop_bit - start_bit) - 1
+    mask = base_mask << start_bit
+    return mask & x >> start_bit
+
